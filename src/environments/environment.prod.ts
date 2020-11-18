d25f11ea8e481cc5256b9b9e88e92bd83c496cb0
Ankit Kumar <ankiitkumar07@gmail.com>
2020-11-18 12:54:26 +0530

    adding firebase conf to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyB4j935b6rCqXuD-FCnr0MOrJFMfjrLtHA",
+    authDomain: "rajbika-store.firebaseapp.com",
+    databaseURL: "https://rajbika-store.firebaseio.com",
+    projectId: "rajbika-store",
+    storageBucket: "rajbika-store.appspot.com",
+    messagingSenderId: "548607642026",
+    appId: "1:548607642026:web:c6710511002679628103d3",
+    measurementId: "G-Q4NCJRJ3H7"
+  }
 };
