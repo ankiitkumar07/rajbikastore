003353534bf051f4289f88dd76646082b2a58603
Ankit Kumar <ankiitkumar07@gmail.com>
2020-12-14 12:52:31 +0530

    prod env firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-  	apiKey: "AIzaSyB4j935b6rCqXuD-FCnr0MOrJFMfjrLtHA",
-    authDomain: "rajbika-store.firebaseapp.com",
-    databaseURL: "https://rajbika-store.firebaseio.com",
-    projectId: "rajbika-store",
-    storageBucket: "rajbika-store.appspot.com",
-    messagingSenderId: "548607642026",
-    appId: "1:548607642026:web:c6710511002679628103d3",
-    measurementId: "G-Q4NCJRJ3H7"
+  	apiKey: "AIzaSyAfVSI5mejEdqWTcT9CuxP5F5W_vxewtOI",
+    authDomain: "rajbika-store-m.firebaseapp.com",
+    projectId: "rajbika-store-m",
+    storageBucket: "rajbika-store-m.appspot.com",
+    messagingSenderId: "314421973131",
+    appId: "1:314421973131:web:73e2ca7cf440cc8127448e",
+    measurementId: "G-RVRJJ5G0CJ"
   }
 };
