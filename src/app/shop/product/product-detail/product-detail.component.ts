ad6e442a5fd2ce6440c70305147280c5ac27896c
Ankit Kumar <ankiitkumar07@gmail.com>
2020-12-18 15:38:48 +0530

    Product detail UI

--- src/app/shop/product/product-detail/product-detail.component.ts
+++ src/app/shop/product/product-detail/product-detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/shared/model/product.model';
+import { FirebaseService } from 'src/app/shared/service/firebase.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit { 
 
-  constructor() { }
+  product: Product;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _firebaseService: FirebaseService
+  ) { }
 
   ngOnInit(): void {
+
+    let productId = this.activatedRoute.snapshot.params.productId;
+    this._firebaseService.getProduct(productId).subscribe((product: Product) => {
+      this.product = product
+    })
   }
 
 }
