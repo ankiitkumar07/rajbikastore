e05c451bd84c92095a74d1dbc5093127dea60dc9
Ankit Kumar <ankiitkumar07@gmail.com>
2021-01-13 20:45:19 +0530

    express config change

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/raj-bika'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/raj-bika/index.html'));
+app.get('/*', function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://test.cashfree.com/billpay/checkout/post/submit');
+    res.sendFile(path.join(__dirname + '/dist/raj-bika/index.html'));
+
+    next();
 });
 
 // Start the app by listening on the default Heroku port
